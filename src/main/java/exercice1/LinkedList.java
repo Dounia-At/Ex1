28166137b4b0be4e3e835cce52841c281ce15bcd
Dounia-At <atoini.dounia@gmail.com>
2023-03-26 23:23:48 +0000

    Exercice1.2

---
+++ src/main/java/exercice1/LinkedList.java
@@ -0,0 +1,17 @@
+package exercice1;
+
+
+public class LinkedList {
+	Node head;
+	
+	public static class Node {
+	    int value;
+
+	    Node next;
+
+	    Node(int d) {
+	      value = d;
+	      next = null;
+	    }
+	  }
+}
--- src/main/java/exercice1/Test.java
+++ src/main/java/exercice1/Test.java
@@ -2,6 +2,8 @@ package exercice1;
 
 import java.util.Scanner;
 
+import exercice1.LinkedList.Node;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -15,6 +17,22 @@ public class Test {
             System.out.println("Number : " + palindrome + " is not a palindrome");
         }     
 		
+        
+        //test linked list
+        LinkedList linkedList = new LinkedList();
+
+	    linkedList.head = new Node(1);
+	    Node second = new Node(2);
+	    Node third = new Node(3);
+
+	    linkedList.head.next = second;
+	    second.next = third;
+
+	    System.out.print("LinkedList: ");
+	    while (linkedList.head != null) {
+	      System.out.print(linkedList.head.value + " ");
+	      linkedList.head = linkedList.head.next;
+	    }
 	}
 
 }
